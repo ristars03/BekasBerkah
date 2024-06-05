983467077df01df0365c20ae1822d46439529ed4
ristars-03 <ristabellaa03@gmail.com>
2024-06-05 09:21:38 +0700

    foto barang

--- Frontend/src/components/TB_FotoBarang.jsx
+++ Frontend/src/components/TB_FotoBarang.jsx
@@ -13,9 +13,81 @@ export const TB_FotoBarang = () => {
                 <div className="w-[340px] px-4 text-align text-zinc-700">
                     Ratio gambar 1:1, pastikan gambar kamu bagus dan jelas.
                 </div>
-                    
+                <div className="Frame1171279036 w-[951.20px] h-[140px] justify-between items-center inline-flex px-10 mt-[-32px]">
+                    <div className="PhotoHover w-[140px] h-[140px] p-[11.20px] bg-white rounded-[11.20px] border border-emerald-200 flex-col justify-center items-center inline-flex">
+                        <div className="MynauiImage w-14 h-14 relative">
+                            <div className="Group w-[42px] h-[42px] left-[7px] top-[7px] absolute">
+                            </div>
+                            <div className="IconParkSolidAddOne w-[22.40px] h-[22.40px] left-[33.60px] top-[0.35px] absolute">
+                                <div className="Rectangle657 w-[10.73px] h-[10.27px] left-[5.60px] top-[6.07px] absolute bg-white" />
+                                <div className="MaskGroup w-[18.67px] h-[18.67px] left-[1.87px] top-[1.87px] absolute">
+                                    <div className="Group w-[18.67px] h-[18.67px] left-0 top-0 absolute">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="FotoUtama1 text-justify text-emerald-200 text-[11.20px] font-normal font-['Roboto'] leading-[14.78px] tracking-tight">Foto Utama 1</div>
+                    </div>
+                    <div className="PhotoHover w-[140px] h-[140px] p-[11.20px] bg-white rounded-[11.20px] border border-emerald-200 flex-col justify-center items-center inline-flex">
+                        <div className="MynauiImage w-14 h-14 relative">
+                            <div className="Group w-[42px] h-[42px] left-[7px] top-[7px] absolute">
+                            </div>
+                            <div className="IconParkSolidAddOne w-[22.40px] h-[22.40px] left-[33.60px] top-[0.35px] absolute">
+                                <div className="Rectangle657 w-[10.73px] h-[10.27px] left-[5.60px] top-[6.07px] absolute bg-white" />
+                                <div className="MaskGroup w-[18.67px] h-[18.67px] left-[1.87px] top-[1.87px] absolute">
+                                    <div className="Group w-[18.67px] h-[18.67px] left-0 top-0 absolute">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="FotoUtama1 text-justify text-emerald-200 text-[11.20px] font-normal font-['Roboto'] leading-[14.78px] tracking-tight">Foto 2</div>
+                    </div>
+                    <div className="PhotoHover w-[140px] h-[140px] p-[11.20px] bg-white rounded-[11.20px] border border-emerald-200 flex-col justify-center items-center inline-flex">
+                        <div className="MynauiImage w-14 h-14 relative">
+                            <div className="Group w-[42px] h-[42px] left-[7px] top-[7px] absolute">
+                            </div>
+                            <div className="IconParkSolidAddOne w-[22.40px] h-[22.40px] left-[33.60px] top-[0.35px] absolute">
+                                <div className="Rectangle657 w-[10.73px] h-[10.27px] left-[5.60px] top-[6.07px] absolute bg-white" />
+                                <div className="MaskGroup w-[18.67px] h-[18.67px] left-[1.87px] top-[1.87px] absolute">
+                                    <div className="Group w-[18.67px] h-[18.67px] left-0 top-0 absolute">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="FotoUtama1 text-justify text-emerald-200 text-[11.20px] font-normal font-['Roboto'] leading-[14.78px] tracking-tight">Foto 3</div>
+                    </div>
+                    <div className="PhotoHover w-[140px] h-[140px] p-[11.20px] bg-white rounded-[11.20px] border border-emerald-200 flex-col justify-center items-center inline-flex">
+                        <div className="MynauiImage w-14 h-14 relative">
+                            <div className="Group w-[42px] h-[42px] left-[7px] top-[7px] absolute">
+                            </div>
+                            <div className="IconParkSolidAddOne w-[22.40px] h-[22.40px] left-[33.60px] top-[0.35px] absolute">
+                                <div className="Rectangle657 w-[10.73px] h-[10.27px] left-[5.60px] top-[6.07px] absolute bg-white" />
+                                <div className="MaskGroup w-[18.67px] h-[18.67px] left-[1.87px] top-[1.87px] absolute">
+                                    <div className="Group w-[18.67px] h-[18.67px] left-0 top-0 absolute">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="FotoUtama1 text-justify text-emerald-200 text-[11.20px] font-normal font-['Roboto'] leading-[14.78px] tracking-tight">Foto 4</div>
+                    </div>
+                    <div className="PhotoHover w-[140px] h-[140px] p-[11.20px] bg-white rounded-[11.20px] border border-emerald-200 flex-col justify-center items-center inline-flex">
+                        <div className="MynauiImage w-14 h-14 relative">
+                            <div className="Group w-[42px] h-[42px] left-[7px] top-[7px] absolute">
+                            </div>
+                            <div className="IconParkSolidAddOne w-[22.40px] h-[22.40px] left-[33.60px] top-[0.35px] absolute">
+                                <div className="Rectangle657 w-[10.73px] h-[10.27px] left-[5.60px] top-[6.07px] absolute bg-white" />
+                                <div className="MaskGroup w-[18.67px] h-[18.67px] left-[1.87px] top-[1.87px] absolute">
+                                    <div className="Group w-[18.67px] h-[18.67px] left-0 top-0 absolute">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="FotoUtama1 text-justify text-emerald-200 text-[11.20px] font-normal font-['Roboto'] leading-[14.78px] tracking-tight">Foto 5</div>
+                    </div>
                 </div>
+                
             </div>
+        </div>
     );
 };
 export default TB_FotoBarang
