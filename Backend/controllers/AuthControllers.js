166a8834f4c4be1cdd472eeae1b534760ea5fb6f
ristars-03 <ristabellaa03@gmail.com>
2024-06-25 00:57:35 +0700

    profile

--- Backend/controllers/AuthControllers.js
+++ Backend/controllers/AuthControllers.js
@@ -1,3 +1,106 @@
+const userModels = require ('../models/UserModels.js')
+const bcrypt = require ("bcryptjs")
+const jwt = require ("jsonwebtoken")
+
+const register = async (req, res) => {
+
+    const { body } = req;
+
+    if (!body.email ||!body.username ||!body.password) {
+        return res.status(400).json({
+            message: 'Data yang dikirim tidak lengkap atau tidak sesuai format.'
+        });
+    }
+
+    try {
+        
+        await userModels.registerUser(body);
+
+        res.status(201).json({
+            message: 'Register berhasil!',
+            data: body
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error!",
+            serverMessage: error
+        });
+    }
+}
+
+const login = async (req, res) => {
+
+    console.log(req.body);
+    const username = req.body.username
+    const password = req.body.password
+
+    try{
+        console.log();
+        let user = await userModels.getUserByUsername(username)
+        user = user[0][0]
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        const checkCorrectPassword = password === user.password
+
+        //if password is wrong
+        if(!checkCorrectPassword){
+            return res.status(401).json({
+                success: false,
+                message: "Incorrect email or password"
+            })
+        }
+
+        const {password: hashedPassword, role, ...rest} = user
+
+        console.log(process.env);
+        //create jwt token
+        const token = jwt.sign(
+            {id: user.id},
+            'gahg48589a45ajfjAUFAHHFIhufuu',
+            {expiresIn: "15d"}
+        )
+
+        res
+        .cookie("accessToken", token, {
+            httpOnly: true,
+            expires: token.expiresIn,
+        })
+        .status(200)
+        .json({
+            message: "Login Success!",
+            token,
+            data:{...rest},
+            role,
+        })
+
+    }catch(err){
+        console.error(err);  // log the error
+        res
+        .status(500)
+        .json({success: false, message: "Failed to log in"})
+    }
+}
+
+const getProfile = async (req, res) => {
+    try {
+        const Username = req.Username;
+        const user = await userModels.getUserByUsername(Username);
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+        res.json({ data: user });
+    } catch (error) {
+        console.error("Error in getProfile:", error);
+        res.status(500).json({ msg: "Server error" });
+    }
+};
+
+module.exports = {register, login, getProfile}
 const userModels = require('../models/UserModels.js');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -197,4 +300,4 @@ module.exports = { register, login, updateProfile };
 //     }
 // }
 
-module.exports = {register, login}
+// module.exports = {register, login}
